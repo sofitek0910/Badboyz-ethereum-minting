2fbcc9ffa625c7526b8ac910aabc5f73aafb6b01
sunshine09101 <sunshine09101@gmail.com>
2022-01-24 01:24:03 +0100

    page-base finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './page/Home.js';
-import Landing from './page/Landing.js';
-import Utility from './page/Utility.js';
 
 
 const App = () => (
   <div>
       <Router>
         <Routes>
-            <Route exact path="/" element={<Landing/>} />
-            <Route exact path="/home" element={<Home/>} />
-            <Route exact path="/utility" element={<Utility/>} />
+            <Route exact path="/" element={<Home/>} />
         </Routes>    
       </Router>
   </div>
