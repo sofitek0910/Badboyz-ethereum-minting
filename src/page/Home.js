1b873e7d2bda08afb7114595c755169e28b9cf9f
sunshine09101 <sunshine09101@gmail.com>
2022-01-25 04:02:26 +0100

    slick carousel finished except responsive

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,25 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "../redux/blockchain/blockchainActions";
 import { fetchData } from "../redux/data/dataActions";
 import swal from "sweetalert";
+import $ from "jquery";
+// import "slick-slider";
 
 function Home() {
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [toggleMenu, setToggleMenu] = useState(false);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [scrollY, setScrollY] = useState(window.innerHeight);
   const [walletAddress, setWallet] = useState("");
   const [claimingNft, setClaimingNft] = useState(false);
   const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
   const [mintAmount, setMintAmount] = useState(1);
-  const [welcomeHeight, setWelcomeHeight] = useState(0);
-  const [buyHeight, setBuyHeight] = useState(0);
-  const [specsHeight, setSpecsHeight] = useState(0);
-  const [roadmapHeight, setRoadmapHeight] = useState(0);
-  const [communityHeight, setCommunityHeight] = useState(0);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
@@ -109,36 +104,27 @@ function Home() {
     const config = await configResponse.json();
     SET_CONFIG(config);
   };
-  
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
   })
 
+  const flyObj = useRef();
+
   const handleScroll = () => {
-    setScrollY(window.scrollY);
-    setWelcomeHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()-250);
-    setBuyHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()-250);
-    setSpecsHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()-50);
-    setRoadmapHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()+$(".spec-section").height());
-    setCommunityHeight($(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height()+$(".welcome-section").height()+$(".buy-section").height()+$(".spec-section").height()+$(".roadmap-section").height()+$(".team-section").height()+250);
-    // console.log("scroll--->", window.scrollY)
-    console.log('scroll event', $(".top-section").height()+$(".navbar-block").height()+$(".hero-block").height());
+    var flyObjDimension = flyObj.current.getBoundingClientRect();
+    if(flyObjDimension.top >= 0 && flyObjDimension.bottom <= window.innerHeight) {
+		console.log('Element is fully visible in screen');
+	}
+    console.log("handle==>", flyObjDimension.top, flyObjDimension.bottom, window.innerHeight)
+
   }
-  
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', changeWidth);
-    return () => {
-        window.removeEventListener('resize', changeWidth)
-    }
-  }, []);
 
   useEffect(() => {
     getConfig();
   }, []);
+
 
   useEffect(() => {
     getData();
@@ -152,14 +138,52 @@ function Home() {
         <div className="badboy">
             <img className="width-100" src="./assets/images/badboyz.jpg"/>
         </div>
-        <div className="our-story">
-            <img className="width-100" src="./assets/images/origin_story_back.jpg"/>
-        </div>
         <div className="road-map">
-            <img className="width-100" src="./assets/images/roadmap.jpg"/>
+            <img className="width-100 roadmap-image" src="./assets/images/roadmap.jpg"/>
+            <img className="width-100 roadmap-front-image" src="./assets/images/roadmap_front.png"/>
+            <div className="text-center">
+                <img className="fly" ref={flyObj} src="./assets/images/fly.png"/>
+            </div>
         </div>
         <div className="origin-story">
-            <img className="width-100" src="./assets/images/origin_story_back.jpg"/>
+            {/* <img className="width-100 origin-story-back" src="./assets/images/origin_story_back.jpg"/> */}
+            <div className="origin-title text-center">
+                <img className="origin-story-title" src="./assets/images/origin_story_title.png"/>
+            </div>
+            <div className="wrapper">
+                <div className="content">
+                    <div className="slider">
+                        <div className="slider__item">
+                            <a>
+                                <img className="origing-image" src="./assets/images/SpiderBoy.png" />
+                            </a>
+                        </div>
+                        <div className="slider__item">
+                            <a href="javascript:;">
+                                <img className="origing-image" src="./assets/images/BatBoy.png" /> 
+                            </a>
+                            
+                        </div>
+                        <div className="slider__item">
+                            <a href="javascript:;">
+                                <img className="origing-image" src="./assets/images/SpiderBoy.png" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="gallery">
+               <div className="slider__item">
+      <img src="./assets/images/SpiderBoy.png" />
+    </div>
+    <div className="slider__item">
+      <img src="./assets/images/BatBoy.png" /> 
+    </div>
+    <div className="slider__item">
+      <img src="./assets/images/SpiderBoy.png" />
+    </div>
+                <div className="gallery-controls"></div>
+            </div> */}
         </div>
         <div className="join-us">
             <img className="width-100" src="./assets/images/join_us.jpg"/>
